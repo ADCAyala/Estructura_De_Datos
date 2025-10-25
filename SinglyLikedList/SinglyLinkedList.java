3f1c33d823484e3318bf41d92eee57bad2d7ebaf
adca <20243ds161@utez.edu.mx>
2025-10-24 20:45:54 -0600

    Agregado Singly Linked List

--- SinglyLikedList/SinglyLinkedList.java
+++ SinglyLikedList/SinglyLinkedList.java
@@ -1,53 +1,82 @@
 public class SinglyLinkedList {
+
     Node head;
 
     public void add(int data){
-        Node newNode=new Node(data);//Creacion de un nuevo Nodo
-        if(head==null){//verificacion de lista vacia
-            head=newNode;
+        Node newNode = new Node(data);//Creación de un nuevo nodo
+        if (head == null) {//Verficación de lista vacia
+            head = newNode;
             return;
         }
-        Node current=head;
-        while(current.next!=null){//Recorrido de los nodos, hasta encontrar el ultimo
-            current=current.next;
-        }
-        current.next=newNode;//insertar nuevo nodo al final de la lista
 
+        Node current = head;
+        while (current.next != null) {//Recorrido de los nodos, hasta encontrar el ultimo
+            
+            current = current.next;
+        }
+
+        current.next = newNode;//Insertar nuevo nodo al final de la lista
     }
-    
-    public boolean contains(int data){
-        
-        Node current=head;
-        while(current!=null){
-            if(current.data==data){
+
+    public Boolean contains(int data){
+        /*if (head == null) {
+            System.out.println("La lista esta vacia.");
+            return false;
+        }*/
+
+        Node current = head;
+        while (current != null) {
+            if (current.data == data) {
                 return true;
             }
-            current=current.next;
+            current = current.next;
+            
         }
+
         return false;
     }
 
     public void remove(int data){
-        if(head==null){//pregunta si la lista está vacía
-            return;
-        }
-        if(head.data==data){//valida que el dato esté en head
+
+        if (head == null) {//Pregunta si la lista esta vacia
             return;
         }
 
-        Node current=head;
-        while(current!=null && current.next.data!=data){//recorre hasta encontrar el valor
-            current=current.next;
+        if (head.data == data) {//Valida que el dato este en el head
+            head = head.next;//Elimina el primer nodo
+            return;
         }
-        current.next=current.next.next;//asignamos el nodo corriente al siguiente
+
+        Node current = head;
+
+        while (current.next != null && current.next.data != data) {//Recorrer hasta encontrar
+            current = current.next;
+        }
+
+        current.next = current.next.next;//ASignamos el nodo corriente al siguiente
     }
 
     public void printList(){
         Node current = head;
-        while (current!=null){
+        while (current != null) {
             System.out.println(current.data);
-            current=current.next;
+            current = current.next;
         }
-        System.out.println("null");
+
+        System.out.println("Null");
+    }
+
+
+    public int numberOccurrences(int data){
+
+        int contador = 0;
+        Node current = head;
+        while (current != null) {
+            if (current.data == data) {
+                contador++;
+            }
+            current = current.next;
+        }
+        return contador;
     }
 }
--- SinglyLikedList/app.java
+++ SinglyLikedList/app.java
@@ -1,13 +1,19 @@
-public class app{
-    public static void main(String[] args)throws Exception {
+public class App {
+    public static void main(String[] args) throws Exception {
 
-        SinglyLinkedList=new SinglyLinkedList();
+        SinglyLinkedList lista = new SinglyLinkedList();
+        lista.printList();
         lista.add(1);
         lista.add(2);
+        lista.add(3);
         lista.add(5);
-        lista.add(6);
-        System.out.println("----------------");
+        lista.add(1);
+        System.out.println("==============");
         lista.printList();
-        
+        lista.remove(5);
+        System.out.println("==============");
+        lista.printList();
+        System.out.println("==============");
+        System.out.println(lista.numberOccurrences(1));
     }
 }
